cae02f0d91048df46f11de2e7b92141c7695bfda
sgbaird <sterling.baird@icloud.com>
2021-10-30 21:53:23 -0600

    Update run_grayskull.py

--- run_grayskull.py
+++ run_grayskull.py
@@ -2,15 +2,19 @@
 from souschef.recipe import Recipe
 import os
 from os.path import join
+
 import ElMD
 
 name, version = ElMD.__name__, ElMD.__version__
-os.system("grayskull pypi {0}=={1}".format(name, version))
+# os.system("grayskull pypi {0}=={1}".format(name, version))
+os.system("grayskull pypi https://github.com/sgbaird/ElMD.git")
 
 fpath = join(name, "meta.yaml")
 fpath2 = join("scratch", "meta.yaml")
 my_recipe = Recipe(load_file=fpath)
 my_recipe["requirements"]["host"].append("flit")
+del my_recipe["test"]["imports"][0]
+my_recipe["test"]["imports"].append("ElMD")
 
 my_recipe.save(fpath)
 my_recipe.save(fpath2)
